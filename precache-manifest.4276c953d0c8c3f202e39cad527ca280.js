6f77d38c7b34f0d47a4d354fed29bbaa9c3f39ca
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 17:43:34 +0000

    Deploy RichnNL/AquaVitaeWeb to github.com/RichnNL/AquaVitaeWeb.git:gh-pages

---
+++ precache-manifest.4276c953d0c8c3f202e39cad527ca280.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f5da26c353928ca199bdaaacbb2bfb",
+    "url": "/AquaVitaeWeb/index.html"
+  },
+  {
+    "revision": "f6bda100bfe29ef909d8",
+    "url": "/AquaVitaeWeb/static/css/main.de674c87.chunk.css"
+  },
+  {
+    "revision": "c37873e630b06fbaa730",
+    "url": "/AquaVitaeWeb/static/js/2.ba2de2a2.chunk.js"
+  },
+  {
+    "revision": "f6bda100bfe29ef909d8",
+    "url": "/AquaVitaeWeb/static/js/main.1441ee72.chunk.js"
+  },
+  {
+    "revision": "1de624da7218af72a360",
+    "url": "/AquaVitaeWeb/static/js/runtime~main.0c4d50a0.js"
+  },
+  {
+    "revision": "a12e8bff3c8c7819d6ce083b0a07967c",
+    "url": "/AquaVitaeWeb/static/media/Celtic.a12e8bff.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/AquaVitaeWeb/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "c7dcce084c445260a266f92db56f5517",
+    "url": "/AquaVitaeWeb/static/media/OpenSans-Italic.c7dcce08.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/AquaVitaeWeb/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/AquaVitaeWeb/static/media/OpenSans-Regular.629a55a7.ttf"
+  }
+]);
