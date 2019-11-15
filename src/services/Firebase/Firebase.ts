4ba2e4e7c5c614748e043efcceb7fd1ed59d8b87
Dyer,Richard R.J <r.dyer@student.fontys.nl>
2019-11-15 07:22:12 +0100

    Merge

--- src/services/Firebase/Firebase.ts
+++ src/services/Firebase/Firebase.ts
@@ -8,7 +8,7 @@ const config = {
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKETT,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
   };
 
